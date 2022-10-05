6bb9a321d2fc17ae915495b3cae23631748d5a64
Jonas Lekgau <77699400+codewithwest@users.noreply.github.com>
2022-10-05 13:55:12 +0200

    one more

--- lib/const/List_Tile.dart
+++ lib/const/List_Tile.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/theme/theme.dart';
 
 class Square extends StatelessWidget {
   final String title;
   final String subtitle;
   final IconData icon;
+
   //final Icons IconName;
   //final _route;
 
   Square(this.title, this.subtitle, this.icon);
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        title,
+    return Card(
+      child: ListTile(
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        trailing: Icon(icon),
+        onTap: () {},
       ),
-      subtitle: Text(subtitle),
-      trailing: Icon(icon),
-      onTap: () {},
     );
   }
 }
